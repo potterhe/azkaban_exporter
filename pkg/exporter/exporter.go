4339f6f0f2e59462ea5e77e59c245c9628d0edf9
potterhe <potter.he@gmail.com>
2022-01-30 22:04:44 +0800

    add metrics version

--- pkg/exporter/exporter.go
+++ pkg/exporter/exporter.go
@@ -9,7 +9,8 @@ var (
 )
 
 type Exporter struct {
-	up *prometheus.Desc
+	up      *prometheus.Desc
+	version *prometheus.Desc
 }
 
 // New returns an initialized exporter.
@@ -21,6 +22,12 @@ func New() *Exporter {
 			nil,
 			nil,
 		),
+		version: prometheus.NewDesc(
+			prometheus.BuildFQName(namespace, "", "version"),
+			"The version of this azkaban server.",
+			[]string{"version"},
+			nil,
+		),
 	}
 }
 
@@ -28,10 +35,12 @@ func New() *Exporter {
 // implements prometheus.Collector.
 func (e *Exporter) Describe(ch chan<- *prometheus.Desc) {
 	ch <- e.up
+	ch <- e.version
 }
 
 // Collect fetches the statistics from the configured azkaban web server, and
 // delivers them as Prometheus metrics. It implements prometheus.Collector.
 func (e *Exporter) Collect(ch chan<- prometheus.Metric) {
 	ch <- prometheus.MustNewConstMetric(e.up, prometheus.GaugeValue, 0)
+	ch <- prometheus.MustNewConstMetric(e.version, prometheus.GaugeValue, 1, "3.91.0-134-g68e7c718")
 }
